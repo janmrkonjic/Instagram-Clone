c572aed9bc984e897fc7941ec883f5881a0968ea
janmrkonjic <jan.mrkonjic2005@gmail.com>
2024-02-09 20:14:53 +0100

    modal

--- app/javascript/controllers/new_post_form_controller.js
+++ app/javascript/controllers/new_post_form_controller.js
@@ -1,10 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
+import Cropper from 'cropperjs';
 
 // Connects to data-controller="new-post-form"
 export default class extends Controller {
   static targets = ['selectFileButton', 'photoFile'];
 
   connect () {
+    console.log('Connected to new post form controller');
+    console.log('Cropper', Cropper);
   }
 
   selectFile (e) {
